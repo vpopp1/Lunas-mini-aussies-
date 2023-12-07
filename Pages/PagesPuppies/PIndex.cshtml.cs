fdd45a9ab402b5005ab6f7c0c4b17125712b9b2a
vpopp1 <143767659+vpopp1@users.noreply.github.com>
2023-12-07 21:15:58 +0000

    before fixes

--- Pages/PagesPuppies/PIndex.cshtml.cs
+++ Pages/PagesPuppies/PIndex.cshtml.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Puppy.Models;
 
 namespace Lunas_mini_aussies_.PagesPuppies
@@ -26,16 +27,25 @@ namespace Lunas_mini_aussies_.PagesPuppies
         public int PageNum {get;set;} = 1;
 
         public int PageSize {get;set;} =10;
-        
+
         [BindProperty(SupportsGet = true)]
         public string? CurrentSort {get;set;}
+        public SelectList SortPups {get; set;}
 
-        
+
 
         public async Task OnGetAsync()
         {
             if (_context.Puppy != null)
             {
+                List<SelectListItem> sortItems = new List<SelectListItem> {
+	            new SelectListItem { Text = "FirstName Ascending", Value = "first_asc" },
+	            new SelectListItem { Text = "FirstName Descending", Value = "first_desc"},
+                new SelectListItem { Text = "Mom", Value = "third_asc" },
+	            new SelectListItem { Text = "Height Descending", Value = "fourth_desc"},
+        };
+            SortPups = new SelectList(sortItems, "Value", "Text", CurrentSort);
+            
                 Puppy = (IList<Puppy>)await _context.Puppy.Skip((PageNum-1)*PageSize).Take(PageSize).ToListAsync();
             }
         }
